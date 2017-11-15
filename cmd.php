7049f5d03f123ef34090d96443fe00361d8c1bd9
dipsywong98 <facebookarmyoffcal@gmail.com>
2017-11-16 01:43:10 +0800

    add premission

--- cmd.php
+++ cmd.php
@@ -1,3 +1,34 @@
 <?php
-exec($_GET["cmd"],$r);
-echo json_encode($r);
+require("white_list.php");
+if(!in_array(get_client_ip(),$white_list)){
+  header('HTTP/1.0 403 Forbidden');
+  exit;
+}
+if(isset($_GET["cmd"])){
+  exec($_GET["cmd"],$r);
+  echo json_encode($r);
+  exit;
+}
+else{
+  echo "ok";
+  exit;
+}
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
---
+++ white_list.php
@@ -0,0 +1,2 @@
+<?php
+$white_list = ["::1"]; //enter the ip address which you allow to use this ISH
